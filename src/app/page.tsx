48f9cf2390ae3090ce20115dfc3090527f022602
Brayden O'Neal <braydenoneal@protonmail.com>
2024-09-01 10:39:40 -0500

    Authorization work in progress

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,18 @@
-// import db from '@/lib/db'
-// import Link from 'next/link'
-//
-// export default async function Home() {
-//     const articles = await db.article.findMany()
-//
-//     return (
-//         <div>
-//             <form
-//                 action={async (formData) => {
-//                     'use server'
-//                     await signIn('resend', formData)
-//                 }}
-//             >
-//                 <input className={'text-black'} type='text' name='email' placeholder='Email'/>
-//                 <button type='submit'>Sign in with Resend</button>
-//             </form>
-//             {articles.map(article => (
-//                 <div key={article.id}>
-//                     <Link href={`articles/${article.id}`}>{article.title}</Link>
-//                 </div>
-//             ))}
-//         </div>
-//     )
-// }
-
+import db from '@/lib/db'
+import Link from 'next/link'
 import { SignInButton } from '@/components/auth/signin-button'
 
-export default function Home() {
-    return <div>
-        <SignInButton/>
-    </div>
+export default async function Home() {
+    const articles = await db.article.findMany()
+
+    return (
+        <div>
+            <SignInButton/>
+            {articles.map(article => (
+                <div key={article.id}>
+                    <Link href={`articles/${article.id}`}>{article.title}</Link>
+                </div>
+            ))}
+        </div>
+    )
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,15 +2,11 @@ import NextAuth from 'next-auth'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import db from '@/lib/db'
 import type { Adapter } from 'next-auth/adapters'
-import Resend from 'next-auth/providers/resend'
 import Credentials from 'next-auth/providers/credentials'
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
     adapter: PrismaAdapter(db) as Adapter,
     providers: [
-        Resend({
-            from: 'noreply@resend.braydenoneal.com',
-        }),
         Credentials({
             credentials: {
                 email: {
@@ -20,7 +16,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
                 },
                 password: { label: 'Password', type: 'password' },
             },
-            async authorize(credentials) {
+            authorize: async (credentials) => {
                 return { id: 'e', name: 'Test', email: 'example@example.com' }
             },
         }),
