fb2b23fea07fee4bc852f12b73cee1975a067221
Brayden O'Neal <braydenoneal@protonmail.com>
2024-09-16 16:38:58 -0500

    Redirect to sign in page when signed out

--- backend/src/app/layout.tsx
+++ backend/src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from 'next/font/google'
 import Link from 'next/link'
 import './globals.css'
 import SignedIn from '@/components/auth/signed-in'
-import SignedOut from '@/components/auth/signed-out'
 import {
     NavigationMenu,
     NavigationMenuItem,
@@ -22,45 +21,40 @@ import {
 } from '@/components/ui/dropdown-menu'
 import { ThemeProvider } from '@/components/theme-provider'
 import { ModeToggle } from '@/components/ui/mode-toggle-button'
-import { Separator } from '@/components/ui/separator'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return <html lang='en'>
     <body className={inter.className}>
-    <ThemeProvider
-        attribute='class'
-        defaultTheme='system'
-        enableSystem
-        disableTransitionOnChange
-    >
-        <header className={'border-b'}>
-            <div className={'h-16 flex items-center justify-between mx-auto max-w-[1280px]'}>
-                <div className='flex gap-4 items-center'>
-                    <Link className={'text-lg text-primary font-bold hover:scale-105'} href={'/'}>Drury Mirror</Link>
-                    <NavigationMenu>
-                        <NavigationMenuList>
-                            <SignedIn userHasRoles={['admin']}>
-                                <NavigationMenuItem>
-                                    <Link href='/' legacyBehavior passHref>
-                                        <NavigationMenuLink
-                                            className={navigationMenuTriggerStyle()}>Home</NavigationMenuLink>
-                                    </Link>
-                                </NavigationMenuItem>
-                                <NavigationMenuItem>
-                                    <Link href='/admin/users' legacyBehavior passHref>
-                                        <NavigationMenuLink
-                                            className={navigationMenuTriggerStyle()}>Users</NavigationMenuLink>
-                                    </Link>
-                                </NavigationMenuItem>
-                            </SignedIn>
-                        </NavigationMenuList>
-                    </NavigationMenu>
-                </div>
-                <div className={'flex gap-2 h-10'}>
-                    <ModeToggle/>
-                    <SignedIn>
+    <ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
+        <SignedIn>
+            <header className={'border-b'}>
+                <div className={'h-16 flex items-center justify-between mx-auto max-w-[1280px] px-4'}>
+                    <div className='flex gap-4 items-center'>
+                        <Link className={'text-lg text-primary font-bold hover:scale-105'} href={'/'}>Drury
+                            Mirror</Link>
+                        <NavigationMenu>
+                            <NavigationMenuList>
+                                <SignedIn userHasRoles={['admin']}>
+                                    <NavigationMenuItem>
+                                        <Link href='/' legacyBehavior passHref>
+                                            <NavigationMenuLink
+                                                className={navigationMenuTriggerStyle()}>Home</NavigationMenuLink>
+                                        </Link>
+                                    </NavigationMenuItem>
+                                    <NavigationMenuItem>
+                                        <Link href='/admin/users' legacyBehavior passHref>
+                                            <NavigationMenuLink
+                                                className={navigationMenuTriggerStyle()}>Users</NavigationMenuLink>
+                                        </Link>
+                                    </NavigationMenuItem>
+                                </SignedIn>
+                            </NavigationMenuList>
+                        </NavigationMenu>
+                    </div>
+                    <div className={'flex gap-2 h-10'}>
+                        <ModeToggle/>
                         <DropdownMenu>
                             <DropdownMenuTrigger asChild>
                                 <Button variant={'outline'} size={'icon'}><User
@@ -76,15 +70,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                                 </DropdownMenuItem>
                             </DropdownMenuContent>
                         </DropdownMenu>
-                    </SignedIn>
-                    <SignedOut>
-                        <Separator orientation={'vertical'}/>
-                        <Button variant={'outline'} asChild><Link href={'/auth/sign-in'}>Sign in</Link></Button>
-                        <Button asChild><Link href={'/auth/sign-up'}>Sign up</Link></Button>
-                    </SignedOut>
+                    </div>
                 </div>
-            </div>
-        </header>
+            </header>
+        </SignedIn>
         <main className={'mx-auto max-w-[1280px]'}>{children}</main>
     </ThemeProvider>
     </body>
--- backend/src/app/page.tsx
+++ backend/src/app/page.tsx
@@ -1,7 +1,10 @@
 import db from '@/lib/db'
 import Link from 'next/link'
+import { verifyToken } from '@/lib/auth/token'
 
 export default async function Home() {
+    await verifyToken()
+
     const articles = await db.article.findMany()
 
     return <div>
